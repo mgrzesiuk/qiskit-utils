ba29d31221815e4690aec4f7abbcc2e6d0747c80
mgrzesiuk <marekgrzesiuk99@gmail.com>
2022-03-22 11:20:42 +0000

    specify qiskit version

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='qiskit_utils',
-    version='1.1.1',
+    version='1.1.2',
     license='MIT',
     author="Marek Grzesiuk",
     packages=['qiskit_utils'],
     url='https://github.com/mgrzesiuk/qiskit-utils',
     keywords='utility-methods qiskit',
     install_requires=[
-          'qiskit',
+          'qiskit>=0.34.2',
       ],
     description="package containing utility methods for qiskit like result parsing and instruction insertion for circuits",
     long_description=long_description,
